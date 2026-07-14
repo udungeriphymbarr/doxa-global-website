5025714c335861cc916b2ece373aa3ca76cb7e82
Udungeri Phymbarr <udungeriphymbarr7@gmail.com>
2026-07-14 21:53:45 +0100

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div>
+      <h1>
+        DOXA GLOBAL
+      </h1>
+
+      <p>
+        Website MVP Coming Soon
+      </p>
+    </div>
+  );
+
+}
+
+export default App;
